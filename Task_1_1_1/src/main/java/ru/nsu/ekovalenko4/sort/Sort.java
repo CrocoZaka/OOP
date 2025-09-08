bf88dfcbf2560fa4f0961a30d2982b3783e24a9c
CrocoZaka <CrocoZaka@gmail.com>
2025-09-08 23:23:57 +0700

    fix reviewdog

--- Task_1_1_1/src/main/java/ru/nsu/ekovalenko4/sort/Sort.java
+++ Task_1_1_1/src/main/java/ru/nsu/ekovalenko4/sort/Sort.java
@@ -6,15 +6,17 @@ package ru.nsu.ekovalenko4.sort;
 public class Sort {
 
     static int[] heapsort(int[] arr) {
-        if(arr == null)
+        if (arr == null) {
             return null;
+        }
         int n = arr.length;
 
-        for (int i = n / 2 - 1; i >= 0; i--)
+        for (int i = n / 2 - 1; i >= 0; i--) {
             heapify(arr, n, i);
+        }
 
-        for (int i=n-1; i>=0; i--) {
-            swap(arr,0,i);
+        for (int i = n - 1; i >= 0; i--) {
+            swap(arr,0, i);
             heapify(arr, i, 0);
         }
         return arr;
@@ -25,11 +27,12 @@ public class Sort {
         int l = 2 * i + 1;
         int r = 2 * i + 2;
 
-        if (l < n && arr[l] > arr[largest])
+        if (l < n && arr[l] > arr[largest]) {
             largest = l;
-        if (r < n && arr[r] > arr[largest])
+        }
+        if (r < n && arr[r] > arr[largest]) {
             largest = r;
-
+        }
         if (largest != i) {
             swap(arr, i, largest);
             heapify(arr, n, largest);
--- Task_1_1_1/src/test/java/ru/nsu/ekovalenko4/sort/SortTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/ekovalenko4/sort/SortTest.java
@@ -12,42 +12,49 @@ class SortTest {
         var result = Sort.heapsort(array);
         assertArrayEquals(null, result);
     }
+
     @Test
     void empty() {
         int[] array = {};
         var result = Sort.heapsort(array);
         assertArrayEquals(new int[]{}, result);
     }
+
     @Test
     void single() {
         int[] array = {1};
         var result = Sort.heapsort(array);
         assertArrayEquals(new int[]{1}, result);
     }
+
     @Test
     void array() {
         int[] array = {2, 7, 4, 5, 1, 3, 8, 6};
         var result = Sort.heapsort(array);
         assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6, 7, 8}, result);
     }
+
     @Test
     void sorted() {
         int[] array = {1, 2, 3, 4, 5, 6, 7, 8};
         var result = Sort.heapsort(array);
         assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6, 7, 8}, result);
     }
+
     @Test
     void duplicates() {
         int[] array = new int[]{1, 2, 3, 1, 2, 3, 1, 2, 3};
         var result = Sort.heapsort(array);
         assertArrayEquals(new int[]{1, 1, 1, 2, 2, 2, 3, 3, 3}, result);
     }
+
     @Test
     void negative() {
         int[] array = new int[]{-1, -5, -7, -2, -8, -3, -4, -6};
         var result = Sort.heapsort(array);
         assertArrayEquals(new int[]{-8, -7, -6, -5, -4, -3, -2, -1}, result);
     }
+    
     @Test
     void big_numbers() {
         int n = 10000000;
