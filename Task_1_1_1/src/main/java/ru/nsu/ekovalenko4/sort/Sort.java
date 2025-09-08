9e46ddb1c872ac5b2b5ddfc274fea54f068bfe7f
CrocoZaka <CrocoZaka@gmail.com>
2025-09-08 23:16:42 +0700

    implement heapsort + add tests + script

--- Task_1_1_1/src/main/java/ru/nsu/ekovalenko4/sort/Sort.java
+++ Task_1_1_1/src/main/java/ru/nsu/ekovalenko4/sort/Sort.java
@@ -1,10 +1,45 @@
 package ru.nsu.ekovalenko4.sort;
 
 /**
- * Heapsort method
+ * Heapsort method.
  */
 public class Sort {
-    public static int[] sort(int[] array) {
-        return array;
+
+    static int[] heapsort(int[] arr) {
+        if(arr == null)
+            return null;
+        int n = arr.length;
+
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(arr, n, i);
+
+        for (int i=n-1; i>=0; i--) {
+            swap(arr,0,i);
+            heapify(arr, i, 0);
+        }
+        return arr;
     }
+
+    private static void heapify(int[] arr, int n, int i) {
+        int largest = i;
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
+
+        if (l < n && arr[l] > arr[largest])
+            largest = l;
+        if (r < n && arr[r] > arr[largest])
+            largest = r;
+
+        if (largest != i) {
+            swap(arr, i, largest);
+            heapify(arr, n, largest);
+        }
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
 }
--- Task_1_1_1/src/test/java/ru/nsu/ekovalenko4/sort/SortTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/ekovalenko4/sort/SortTest.java
@@ -7,9 +7,58 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 class SortTest {
 
     @Test
-    void sort() {
-        int[] array = new int[]{1, 2, 3};
-        var result = Sort.sort(array);
-        assertArrayEquals(new int[]{1, 2, 3},result);
+    void nul() {
+        int[] array = null;
+        var result = Sort.heapsort(array);
+        assertArrayEquals(null, result);
     }
+    @Test
+    void empty() {
+        int[] array = {};
+        var result = Sort.heapsort(array);
+        assertArrayEquals(new int[]{}, result);
+    }
+    @Test
+    void single() {
+        int[] array = {1};
+        var result = Sort.heapsort(array);
+        assertArrayEquals(new int[]{1}, result);
+    }
+    @Test
+    void array() {
+        int[] array = {2, 7, 4, 5, 1, 3, 8, 6};
+        var result = Sort.heapsort(array);
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6, 7, 8}, result);
+    }
+    @Test
+    void sorted() {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8};
+        var result = Sort.heapsort(array);
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6, 7, 8}, result);
+    }
+    @Test
+    void duplicates() {
+        int[] array = new int[]{1, 2, 3, 1, 2, 3, 1, 2, 3};
+        var result = Sort.heapsort(array);
+        assertArrayEquals(new int[]{1, 1, 1, 2, 2, 2, 3, 3, 3}, result);
+    }
+    @Test
+    void negative() {
+        int[] array = new int[]{-1, -5, -7, -2, -8, -3, -4, -6};
+        var result = Sort.heapsort(array);
+        assertArrayEquals(new int[]{-8, -7, -6, -5, -4, -3, -2, -1}, result);
+    }
+    @Test
+    void big_numbers() {
+        int n = 10000000;
+        int[] array = new int[n];
+        int[] expected = new int[n];
+        for (int i = 0; i < n; i++) {
+            expected[i] = i + 1;
+            array[i] = n - i;
+        }
+        Sort.heapsort(array);
+        assertArrayEquals(expected, array);
+    }
+
 }
