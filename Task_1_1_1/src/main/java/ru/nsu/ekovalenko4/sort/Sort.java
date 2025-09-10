e66ebae205a7db48e28053e192dd662d3ac3b941
CrocoZaka <CrocoZaka@gmail.com>
2025-09-10 17:04:35 +0700

    remove return from heapsort method + add graph

--- Task_1_1_1/src/main/java/ru/nsu/ekovalenko4/sort/Sort.java
+++ Task_1_1_1/src/main/java/ru/nsu/ekovalenko4/sort/Sort.java
@@ -1,25 +1,24 @@
 package ru.nsu.ekovalenko4.sort;
 
+import java.util.Arrays;
+import java.util.Random;
+
 /**
  * Heapsort method.
  */
 public class Sort {
 
-    static int[] heapsort(int[] arr) {
-        if (arr == null) {
-            return null;
+    static void heapsort(int[] arr) {
+        if (arr != null) {
+            int n = arr.length;
+            for (int i = n / 2 - 1; i >= 0; i--) {
+                heapify(arr, n, i);
+            }
+            for (int i = n - 1; i >= 0; i--) {
+                swap(arr, 0, i);
+                heapify(arr, i, 0);
+            }
         }
-        int n = arr.length;
-
-        for (int i = n / 2 - 1; i >= 0; i--) {
-            heapify(arr, n, i);
-        }
-
-        for (int i = n - 1; i >= 0; i--) {
-            swap(arr,0, i);
-            heapify(arr, i, 0);
-        }
-        return arr;
     }
 
     private static void heapify(int[] arr, int n, int i) {
@@ -43,6 +42,31 @@ public class Sort {
         int tmp = arr[i];
         arr[i] = arr[j];
         arr[j] = tmp;
+    }
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[] sizes = {10, 50, 100, 500, 1000, 2500, 5000, 7500, 10000, 25000, 50000, 75000, 100000, 250000, 500000, 750000, 1000000};
+
+        for (int size : sizes) {
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++) {
+                array[i] = random.nextInt();
+            }
+            int[] copy = array.clone();
+
+            long start = System.nanoTime();
+            heapsort(array);
+            double time = (System.nanoTime() - start) / 1_000_000.0; // milliseconds
+
+            Arrays.sort(copy);
+            if(Arrays.equals(array, copy)) {
+                System.out.println("Array length: " + size + " - Sorting time: " + time + " ms");
+            }
+            else {
+                System.out.println("Incorrect heapsort result");
+            }
+        }
     }
 
 }
--- Task_1_1_1/src/test/java/ru/nsu/ekovalenko4/sort/SortTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/ekovalenko4/sort/SortTest.java
@@ -9,52 +9,52 @@ class SortTest {
     @Test
     void nul() {
         int[] array = null;
-        var result = Sort.heapsort(array);
-        assertArrayEquals(null, result);
+        Sort.heapsort(array);
+        assertArrayEquals(null, array);
     }
 
     @Test
     void empty() {
         int[] array = {};
-        var result = Sort.heapsort(array);
-        assertArrayEquals(new int[]{}, result);
+        Sort.heapsort(array);
+        assertArrayEquals(new int[]{}, array);
     }
 
     @Test
     void single() {
         int[] array = {1};
-        var result = Sort.heapsort(array);
-        assertArrayEquals(new int[]{1}, result);
+        Sort.heapsort(array);
+        assertArrayEquals(new int[]{1}, array);
     }
 
     @Test
     void array() {
         int[] array = {2, 7, 4, 5, 1, 3, 8, 6};
-        var result = Sort.heapsort(array);
-        assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6, 7, 8}, result);
+        Sort.heapsort(array);
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6, 7, 8}, array);
     }
 
     @Test
     void sorted() {
         int[] array = {1, 2, 3, 4, 5, 6, 7, 8};
-        var result = Sort.heapsort(array);
-        assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6, 7, 8}, result);
+        Sort.heapsort(array);
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6, 7, 8}, array);
     }
 
     @Test
     void duplicates() {
         int[] array = new int[]{1, 2, 3, 1, 2, 3, 1, 2, 3};
-        var result = Sort.heapsort(array);
-        assertArrayEquals(new int[]{1, 1, 1, 2, 2, 2, 3, 3, 3}, result);
+        Sort.heapsort(array);
+        assertArrayEquals(new int[]{1, 1, 1, 2, 2, 2, 3, 3, 3}, array);
     }
 
     @Test
     void negative() {
         int[] array = new int[]{-1, -5, -7, -2, -8, -3, -4, -6};
-        var result = Sort.heapsort(array);
-        assertArrayEquals(new int[]{-8, -7, -6, -5, -4, -3, -2, -1}, result);
+        Sort.heapsort(array);
+        assertArrayEquals(new int[]{-8, -7, -6, -5, -4, -3, -2, -1}, array);
     }
-    
+
     @Test
     void big_numbers() {
         int n = 10000000;
